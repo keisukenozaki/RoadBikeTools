1b2b438bb7076207e96cdfa4c7886b0500a00f95
Keisuke Nozaki <keisukenozaki@gmail.com>
2026-08-17 17:50:38 +0900

    空気圧完成

--- bike-fit-app/src/app/app.routes.ts
+++ bike-fit-app/src/app/app.routes.ts
@@ -1,9 +1,15 @@
 import { Routes } from '@angular/router';
+import { Home } from './home/home';
 import { SaddleCalculator } from './saddle-calculator/saddle-calculator';
 import { KopsCalculator } from './kops-calculator/kops-calculator';
+import { TirePressureCalculator } from './tire-pressure-calculator/tire-pressure-calculator';
+import { FormulaInfo } from './formula-info/formula-info';
 
 export const routes: Routes = [
-  { path: '', component: SaddleCalculator },
+  { path: '', component: Home },
+  { path: 'saddle-height', component: SaddleCalculator },
   { path: 'kops', component: KopsCalculator },
+  { path: 'tire-pressure', component: TirePressureCalculator },
+  { path: 'formulas', component: FormulaInfo },
   { path: '**', redirectTo: '' },
 ];
